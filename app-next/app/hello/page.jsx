1ad817eb6cef86a76e5f8cfc21ca4e0885f6d76f
Tim Burkei <tb169@hdm-stuttgart.de>
2024-06-11 20:15:35 +0200

    add page and app

---
+++ app-next/app/hello/page.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+export default function HydrationExample() {
+  const [count, setCount] = useState(0);
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHydrated(true);
+    }, 1000); //  delay 1 second
+
+    return () => clearTimeout(timer); 
+  }, []); 
+
+  return (
+    <div >
+      <h1>Hydration Beispiel</h1>
+      <button
+        
+        onClick={() => setCount(count + 1)}
+        style={{ visibility: hydrated ? 'visible' : 'hidden' }}
+      >
+        Click me! Count: {count}
+      </button>
+    </div>
+  );
+}
